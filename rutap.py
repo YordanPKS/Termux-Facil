e4dc00c78b648b9babcd1e6f62baa845d8c10eea
YordanPKS <140574806+YordanPKS@users.noreply.github.com>
2024-06-18 14:50:27 -0400

    Update rutap.py

--- rutap.py
+++ rutap.py
@@ -1,3 +1,4 @@
-echo "cd storage/emulated/0/descargasFlix" > /home/flix.sh
-echo "python3 exep.py" >> /home/flix.sh
-cd /home
+cd
+echo "cd storage/emulated/0/descargasFlix" > flix.sh
+echo "python3 exep.py" >> flix.sh
+
